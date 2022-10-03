aa856d60b55a85db62989ed370fd943ba646fb74
Laura Langa <laura.langa@stud.uni-corvinus.hu>
2022-10-03 21:13:01 +0200

    Excel, munkafüzet létrehozása

--- ExcelGeneralas/ExcelGeneralas_week4/Form1.cs
+++ ExcelGeneralas/ExcelGeneralas_week4/Form1.cs
@@ -17,17 +17,47 @@ namespace ExcelGeneralas_week4
         RealEstateEntities context = new RealEstateEntities();      //példányosítás!
         List<Flat> flats;                                           //mi az hogy nem kell inicializálni? 
 
-        Excel.Application xlApp; // A Microsoft Excel alkalmazás
-        Excel.Workbook xlWB; // A létrehozott munkafüzet
-        Excel.Worksheet xlSheet; // Munkalap a munkafüzeten belül
+        Excel.Application xlApp;             // A Microsoft Excel alkalmazás
+        Excel.Workbook xlWB;                 // A létrehozott munkafüzet
+        Excel.Worksheet xlSheet;             // Munkalap a munkafüzeten belül
         public Form1()
         {
             InitializeComponent();
 
             LoadData();
+            CreatExcel();
         }
 
- 
+        private void CreatExcel()
+        {
+            try
+            {
+                // Excel elindítása, applikáció objektum betöltése
+                xlApp = new Excel.Application();
+
+                // Új munkafüzet
+                xlWB = xlApp.Workbooks.Add(Missing.Value);      //Missing:Value?
+
+                // Új munkalap
+                xlSheet = xlWB.ActiveSheet;
+
+                // Control átadása a felhasználónak
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex) // Hibakezelés a beépített hibaüzenettel
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                // Hiba esetén az Excel applikáció bezárása automatikusan
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+        }
+
         private void LoadData()     //adatok átmásolása memóriába adattárból
         {
             flats = context.Flats.ToList();
