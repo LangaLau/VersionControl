bd92af0ab7a286ffa89d63471e807b21b149b7e7
Laura Langa <laura.langa@stud.uni-corvinus.hu>
2022-10-03 21:09:56 +0200

    Excel változók

--- ExcelGeneralas/ExcelGeneralas_week4/Form1.cs
+++ ExcelGeneralas/ExcelGeneralas_week4/Form1.cs
@@ -7,8 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Excel = Microsoft.Office.Interop.Excel;   //fájlba behivatkozás
-using System.Reflection;
+using Excel = Microsoft.Office.Interop.Excel;   //()alias       fájlba behivatkozás 
+using System.Reflection;                 //(technikai könyvtár) de az Objektumot is há kell adni, Add/References.../Excel
 
 namespace ExcelGeneralas_week4
 {
@@ -16,12 +16,18 @@ namespace ExcelGeneralas_week4
     {
         RealEstateEntities context = new RealEstateEntities();      //példányosítás!
         List<Flat> flats;                                           //mi az hogy nem kell inicializálni? 
+
+        Excel.Application xlApp; // A Microsoft Excel alkalmazás
+        Excel.Workbook xlWB; // A létrehozott munkafüzet
+        Excel.Worksheet xlSheet; // Munkalap a munkafüzeten belül
         public Form1()
         {
             InitializeComponent();
+
             LoadData();
         }
 
+ 
         private void LoadData()     //adatok átmásolása memóriába adattárból
         {
             flats = context.Flats.ToList();
