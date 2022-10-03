946ed8fd159faaba3a4d16aa0cc3e898667f932a
Laura Langa <laura.langa@stud.uni-corvinus.hu>
2022-10-03 20:55:49 +0200

    Excel reference

--- ExcelGeneralas/ExcelGeneralas_week4/Form1.cs
+++ ExcelGeneralas/ExcelGeneralas_week4/Form1.cs
@@ -7,14 +7,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace ExcelGeneralas_week4
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();      //példányosítás!
+        List<Flat> flats;                                           //mi az hogy nem kell inicializálni? 
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()     //adatok átmásolása memóriába adattárból
+        {
+            flats = context.Flats.ToList();
         }
     }
 }
