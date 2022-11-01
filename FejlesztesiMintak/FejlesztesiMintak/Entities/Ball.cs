13870146034ce1d248b4c7c004b09f2a5d352dae
Laura Langa <laura.langa@stud.uni-corvinus.hu>
2022-11-01 19:55:56 +0100

    Labda létrehozása

---
+++ FejlesztesiMintak/FejlesztesiMintak/Entities/Ball.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FejlesztesiMintak.Entities
+{
+    class Ball : Label
+    {
+        public Ball()
+        {
+            AutoSize = false;
+            Width = Height = 50;
+            Paint += Ball_Paint;
+
+        }
+        protected void DrawImage(Graphics graphics)
+        {
+            graphics.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+        }
+
+        private void Ball_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+        public void MoveBall()
+        {
+            Left++;
+        }
+    }
+}
+
