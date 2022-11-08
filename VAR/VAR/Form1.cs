1c27d24e09dbd6c5614f9c210181d6b9724ca47e
Laura Langa <laura.langa@stud.uni-corvinus.hu>
2022-11-08 19:00:18 +0100

    kód elemzése

--- VAR/VAR/Form1.cs
+++ VAR/VAR/Form1.cs
@@ -14,27 +14,69 @@ namespace VAR
     public partial class Form1 : Form
     {
         PortfolioEntities context = new PortfolioEntities();
-        List<Tick> ticks;
+        List<Tick> Ticks;
         List<PortfolioItem> Portfolio = new List<PortfolioItem>();
         public Form1()
         {
             InitializeComponent();
 
-            ticks = context.Ticks.ToList();
-            dgvTick.DataSource = ticks;
+            Ticks = context.Ticks.ToList();
+            dgvTick.DataSource = Ticks;
             CreatePortfolio();
         }
 
         private void CreatePortfolio()
         {
-            Portfolio.Add(new PortfolioItem() 
+            Portfolio.Add(new PortfolioItem()
             { Index = "OTP", Volume = 10 });
-            Portfolio.Add(new PortfolioItem() 
+            Portfolio.Add(new PortfolioItem()
             { Index = "MOL", Volume = 10 });
-            Portfolio.Add(new PortfolioItem() 
+            Portfolio.Add(new PortfolioItem()
             { Index = "PICK", Volume = 10 });
 
-            dvgPortfolio.DataSource = "";
+            //dvgPortfolio.DataSource = "";
+
+
+
+            //a. Portfóliónk elemszáma:
+            int elemszám = Portfolio.Count();
+            //A Count() bálrmilyen megszámlálható listára alkalmazható.
+
+            //b. A portfólióban szereplő részvények darabszáma: 
+            decimal részvényekSzáma = (from x in Portfolio select x.Volume).Sum();
+            MessageBox.Show(string.Format("Részvények száma: {0}", részvényekSzáma));
+            //Először egy listába kigyűjtjük csak a darabszámokat, majd az egész bezárójlezett listát summázzuk. 
+            //(A zárójelben lévő LINQ egy int-ekből álló listát ad, mert a Count tulajdonság int típusú.)
+            //Működik a Min(), Max(), Average(), stb. is.
+
+            //c. A legrégebbi kereskedési nap:
+            DateTime minDátum = (from x in Ticks select x.TradingDay).Min();
+
+            //d. A legutolsó kereskedési nap:
+            DateTime maxDátum = (from x in Ticks select x.TradingDay).Max();
+
+            //e. A két dátum közt eltelt idő napokban -- két DateTime típusú objektum különbsége TimeSpan típusú eredményt ad.
+            //A TimeSpan Day tulajdonsága megadja az időtartam napjainak számát. (Nem kell vacakolni a szökőévekkel stb.)
+            int elteltNapokSzáma = (maxDátum - minDátum).Days;
+
+            //f. Az OTP legrégebbi kereskedési napja: 
+            DateTime optMinDátum = (from x in Ticks where x.Index == "OTP" select x.TradingDay).Min();
+
+            //g. Össze is lehet kapcsolni dolgokat, ez már bonyolultabb:
+            var kapcsolt =
+                from
+                    x in Ticks
+                join
+            y in Portfolio
+        on x.Index equals y.Index
+                select new
+                {
+                    Index = x.Index,
+                    Date = x.TradingDay,
+                    Value = x.Price,
+                    Volume = y.Volume
+                };
+            dgvTick.DataSource = kapcsolt.ToList();
         }
     }
 }
