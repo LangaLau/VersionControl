34201f5d1c4ee5d4e9da9100ed514cf2167c828e
Laura Langa <laura.langa@stud.uni-corvinus.hu>
2022-11-08 18:57:19 +0100

    Portfólió tárolása a memóriában

--- VAR/VAR/Form1.Designer.cs
+++ VAR/VAR/Form1.Designer.cs
@@ -29,13 +29,51 @@ namespace VAR
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgvTick = new System.Windows.Forms.DataGridView();
+            this.dvgPortfolio = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTick)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dvgPortfolio)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvTick
+            // 
+            this.dgvTick.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTick.Location = new System.Drawing.Point(12, 12);
+            this.dgvTick.Name = "dgvTick";
+            this.dgvTick.RowHeadersWidth = 51;
+            this.dgvTick.RowTemplate.Height = 24;
+            this.dgvTick.Size = new System.Drawing.Size(280, 410);
+            this.dgvTick.TabIndex = 0;
+            // 
+            // dvgPortfolio
+            // 
+            this.dvgPortfolio.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dvgPortfolio.Location = new System.Drawing.Point(365, 12);
+            this.dvgPortfolio.Name = "dvgPortfolio";
+            this.dvgPortfolio.RowHeadersWidth = 51;
+            this.dvgPortfolio.RowTemplate.Height = 24;
+            this.dvgPortfolio.Size = new System.Drawing.Size(280, 410);
+            this.dvgPortfolio.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dvgPortfolio);
+            this.Controls.Add(this.dgvTick);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTick)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dvgPortfolio)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvTick;
+        private System.Windows.Forms.DataGridView dvgPortfolio;
     }
 }
 
--- VAR/VAR/Form1.cs
+++ VAR/VAR/Form1.cs
@@ -7,15 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using VAR.Entities;
 
 namespace VAR
 {
     public partial class Form1 : Form
     {
-
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> ticks;
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
         public Form1()
         {
             InitializeComponent();
+
+            ticks = context.Ticks.ToList();
+            dgvTick.DataSource = ticks;
+            CreatePortfolio();
+        }
+
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() 
+            { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() 
+            { Index = "MOL", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() 
+            { Index = "PICK", Volume = 10 });
+
+            dvgPortfolio.DataSource = "";
         }
     }
 }
